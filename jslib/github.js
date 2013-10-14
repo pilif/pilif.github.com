b966c051e2f98535e316d2af87697fcf809a9c72
Philip Hofstetter <phofstetter@sensational.ch>
2013-10-14 09:49:22 +0200

    correctly link to repos

--- jslib/github.js
+++ jslib/github.js
@@ -8,7 +8,7 @@
         render = function(repos){
             var i = 0, fragment = '';
             for(i = 0; i < repos.length; i++)
-                fragment += '<li><a href="'+repos[i].url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
+                fragment += '<li><a href="'+repos[i].html_url+'">'+repos[i].name+'</a><p>'+repos[i].description+'</p></li>';
             return fragment;
         };
 
